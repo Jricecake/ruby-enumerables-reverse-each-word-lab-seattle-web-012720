d8b0ae5b0ca030dbc025875254d04228c066e3ac
Jonathan Riecke <jonathanriecke@gmail.com>
2020-01-20 23:15:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,14 +2,16 @@ def reverse_each_word(sentence)
   starting_sentence_array = sentence.split.reverse
   i = 0
   final_sentence = ""
+  reverse_sentence = []
   while i < starting_sentence_array.length do
-    current_word = starting_sentence_array[i].reverse
-    if current_word == starting_sentence_array[-1].reverse
-      final_sentence << "#{current_word}"
-    else
-      final_sentence << "#{current_word} "
-    end
+    reverse_sentence[i] = starting_sentence_array[i].reverse
+#    current_word = starting_sentence_array[i].reverse
+#    if current_word == starting_sentence_array[-1].reverse
+#      final_sentence << "#{current_word}"
+#    else
+#      final_sentence << "#{current_word} "
+#    end
    i += 1
  end
- final_sentence
+ reverse_sentence
 end
